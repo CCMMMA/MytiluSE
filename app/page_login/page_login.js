10e6d180f6fc9f0a9d84f52862bddba5ffb37a1a
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-01-23 13:59:15 +0100

    "Login page and access"

--- app/page_login/page_login.js
+++ app/page_login/page_login.js
@@ -1,19 +1,39 @@
 var PageLoginViewModel = require("./page_login-view-model");
-var view = require("ui/core/view");
-var dialog = require("tns-core-modules/ui/dialogs");
 var Observable = require("data/observable");
-var ObservableArray = require("data/observable-array").ObservableArray;
-var homeViewModel = new PageLoginViewModel();
-var observableModule = require("data/observable");
-const application = require("tns-core-modules/application");
-const frameModule = require("tns-core-modules/ui/frame");
+const httpModule = require("http");
+
 
 function pageLoaded(args) {
     var page = args.object;
-    var login = new Observable.fromObject({
-    });
+    var login = new Observable.fromObject({});
 
     page.bindingContext = login;
 }
 
 exports.pageLoaded = pageLoaded;
+
+exports.OnTapLogin= function(args)
+{
+    const button = args.object;
+    const page = button.page;
+    //page.frame.navigate("page_login/page_login");
+
+    var url = api_base_url + "/user/login";
+    httpModule.request({
+        url: url,
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        content: JSON.stringify({
+            name: "cirogiuseppe.devita01",
+            pass: "Ccmmma2019!!"
+        })
+    }).then((response) =>
+    {
+        const result = response.content.toJSON();
+        console.log(result.roles);
+    },
+        (e) =>
+        {
+            console.log("Error");
+        });
+};

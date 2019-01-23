c8f33b6930e70fdda5c890c1e8d538b7f480eb46
ciro97 <39745066+ciro97@users.noreply.github.com>
2019-01-23 14:47:46 +0100

    "Login page and access"

--- app/page_login/page_login.js
+++ app/page_login/page_login.js
@@ -1,7 +1,7 @@
 var PageLoginViewModel = require("./page_login-view-model");
 var Observable = require("data/observable");
 const httpModule = require("http");
-
+var view = require("ui/core/view");
 
 function pageLoaded(args) {
     var page = args.object;
@@ -18,19 +18,25 @@ exports.OnTapLogin= function(args)
     const page = button.page;
     //page.frame.navigate("page_login/page_login");
 
+    var Username = view.getViewById(page, "username");
+    var user = Username.text;
+
+    var Password = view.getViewById(page, "password");
+    var pass = Password.text;
+
     var url = api_base_url + "/user/login";
     httpModule.request({
         url: url,
         method: "POST",
         headers: { "Content-Type": "application/json" },
         content: JSON.stringify({
-            name: "cirogiuseppe.devita01",
-            pass: "Ccmmma2019!!"
+            name: user,
+            pass: pass
         })
     }).then((response) =>
     {
-        const result = response.content.toJSON();
-        console.log(result.roles);
+        //const result = response.content.toJSON();
+        console.log(response);
     },
         (e) =>
         {
